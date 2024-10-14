74b441be7a0eee871af2757c572e6cb478923254
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-10-14 18:44:08 +0530

    Update states.js

--- plugins/states.js
+++ plugins/states.js
@@ -45,9 +45,7 @@ async function initializeStatusListener(conn) {
             const caption = mek.message.conversation || mek.message.caption || 'No caption provided.';
 
             // Log the output with sender's push name, content type, and caption
-            console.log(`New status posted by 💥: ${senderPushName}
-            Media Type: ${contentType || 'No media'}
-            Caption: ${caption}`);
+            console.log(`New status posted by 💥: ${senderPushName} Media Type: ${contentType || 'No media'} Caption: ${caption}`);
             
 
             // Check the config to decide whether to send the status seen message
