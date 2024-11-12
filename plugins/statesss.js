0be338f99a31465378b1f7f55573ec1b7f360307
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-11-12 13:43:56 +0530

    Update statesss.js

--- plugins/statesss.js
+++ plugins/statesss.js
@@ -120,7 +120,7 @@ async function handleChatUpdate(conn, mek) {
         const config = await readEnv();
 
         // Create the personalized message with config data
-        const personalizedMessage = `*𝗛𝗘𝗬* ${config.pushname || "there"}\n` +
+        const personalizedMessage = `*𝗛𝗘𝗬* ${pushname}\n` +
             `*I am ${config.WCPROFILENAME} 👤*\n` +
             `*From - ${config.WCPROFILEFROM} 📍*\n` +
             `*Age - ${config.WCPROFILEAGE} 🎂*\n` +
