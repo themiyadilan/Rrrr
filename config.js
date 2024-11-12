59126ab8ce41c974909a5ae313171c88a578af7e
Themiya Dilan <128908885+themiyadilann@users.noreply.github.com>
2024-11-12 19:11:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb://mongo:HBzRRSaHLKzREFuawVeBvvWmgBeNszOW@junction.proxy.rlwy.net:40873",
+MONGODB: process.env.MONGODB || "mongodb://mongo:eaoorsmHZSsfRWLEGJjcFKxSTZtoQCLM@junction.proxy.rlwy.net:36842",
 FOOTER: process.env.FOOTER || "Mr Dila",
 BTN: process.env.BTN || "Click Here",
 BTNURL: process.env.BTNURL || "https://whatsapp.com/channel/0029VarK8LEHbFVFcPRvxl3T",
